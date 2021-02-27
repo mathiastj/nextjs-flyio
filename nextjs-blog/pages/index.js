2f62da3ba1e38ad77e083d6627e9cc830ff380d4
mathiastj <2278040+mathiastj@users.noreply.github.com>
2021-02-27 14:16:09 +0100

    next.js blog example

---
+++ nextjs-blog/pages/index.js
@@ -0,0 +1,39 @@
+import Layout, { siteTitle } from '../components/layout'
+import utilStyles from '../styles/utils.module.css'
+
+import { getSortedPostsData } from '../lib/posts'
+
+export async function getStaticProps() {
+  const allPostsData = getSortedPostsData()
+  return {
+    props: {
+      allPostsData
+    }
+  }
+}
+
+
+export default function Home({ allPostsData }) {
+  return (
+    <Layout home>
+      <section className={utilStyles.headingMd}>
+        <p>Some introduction</p>
+      </section>
+      <section className={`${utilStyles.headingMd} ${utilStyles.padding1px}`}>
+        <h2 className={utilStyles.headingLg}>Blog</h2>
+        <ul className={utilStyles.list}>
+          {allPostsData.map(({ id, date, title }) => (
+            <li className={utilStyles.listItem} key={id}>
+              {title}
+              <br />
+              {id}
+              <br />
+              {date}
+            </li>
+          ))}
+        </ul>
+      </section>
+    </Layout>
+  )
+}
+
